6efcc6da5e9d68bb1897a15065cd6b218f27220c
Alex <al.gruen@gmx.de>
2017-07-10 17:07:37 +0200

    javadoc added

--- src/main/java/de/unknownreality/dataframe/DataRow.java
+++ src/main/java/de/unknownreality/dataframe/DataRow.java
@@ -10,12 +10,26 @@ public class DataRow extends UpdateableRow<String,DataFrameHeader,Comparable> {
         super(header,values,index);
     }
 
+    /**
+     * Checks whether a certain value is compatible for a column.
+     * Compatible means that the value has a type suited for the respective class
+     * @param value checked value
+     * @param headerName header name
+     * @return true if value is compatible
+     */
     @Override
     public boolean isCompatible(Comparable value, String headerName) {
         Class<? extends Comparable> type = getHeader().getType(headerName);
         return type.isAssignableFrom(value.getClass());
     }
 
+    /**
+     * Checks whether a certain value is compatible for a column.
+     * Compatible means that the value has a type suited for the respective class
+     * @param value checked value
+     * @param headerIndex header index
+     * @return true if value is compatible
+     */
     @Override
     public boolean isCompatible(Comparable value, int headerIndex) {
         Class<? extends Comparable> type = getHeader().getType(headerIndex);
