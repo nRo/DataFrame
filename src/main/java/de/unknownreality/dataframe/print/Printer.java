3b2b4c96e6cddf47ea579110156800615481798c
Alex <al.gruen@gmx.de>
2018-07-20 16:06:20 +0200

    dataframe printer added

--- src/main/java/de/unknownreality/dataframe/print/Printer.java
+++ src/main/java/de/unknownreality/dataframe/print/Printer.java
@@ -8,6 +8,8 @@ import de.unknownreality.dataframe.io.DataWriter;
 import de.unknownreality.dataframe.io.ReadFormat;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -23,63 +25,115 @@ public class Printer extends DataWriter {
     private String tLeft = "┤";
     private String tRight = "├";
     private String tTop = "┴";
-    private String tBottom= "┬";
+    private String tBottom = "┬";
     private String innerVerticalLine = "│";
     private String innerHorizontalLine = "─";
     private String innerCrossConnection = "┼";
 
 
-
     @Override
     public void write(BufferedWriter writer, DataContainer<?, ?> dataContainer) {
         try {
+            writeHeader(dataContainer, writer);
             StringBuilder contentLineSb = new StringBuilder();
             StringBuilder topLineSb = new StringBuilder();
             StringBuilder lastLineSb = new StringBuilder();
-            boolean first = true;
-            for (Row row : dataContainer) {
+            boolean last;
+            Iterator<? extends Row> it = dataContainer.iterator();
+            while (it.hasNext()) {
+                Row<?,?> row = it.next();
                 contentLineSb.setLength(0);
                 topLineSb.setLength(0);
-                lastLineSb.setLength(0);
+                last = !it.hasNext();
                 for (int i = 0; i < row.size(); i++) {
                     if (i == 0) {
                         contentLineSb.append(leftLine);
-                        topLineSb.append(first ? topLeftCorner : tRight);
-                        lastLineSb.append(bottomLeftCorner);
-
+                        topLineSb.append(tRight);
+                        if (last) {
+                            lastLineSb.append(bottomLeftCorner);
+                        }
                     }
-                    Object v = row.get(i);
-                    String content = String.format("%-12.10s", v);
+                    String content = formatContent(row, i);
                     contentLineSb.append(content);
                     for (int j = 0; j < content.length(); j++) {
-                        topLineSb.append(first ? topLine : innerHorizontalLine);
-                        lastLineSb.append(bottomLine);
+                        topLineSb.append(innerHorizontalLine);
+                        if (last) {
+                            lastLineSb.append(bottomLine);
+                        }
                     }
                     if (i != row.size() - 1) {
                         contentLineSb.append(innerVerticalLine);
-                        topLineSb.append(first ? tBottom : innerCrossConnection);
-                        lastLineSb.append(tTop);
+                        topLineSb.append(innerCrossConnection);
+                        if (last) {
+                            lastLineSb.append(tTop);
+                        }
                     } else {
                         contentLineSb.append(rightLine);
-                        topLineSb.append(first ? topRightCorner : tLeft);
-                        lastLineSb.append(bottomRightCorner);
+                        topLineSb.append(tLeft);
+                        if (last) {
+                            lastLineSb.append(bottomRightCorner);
+                        }
                     }
                 }
                 writer.write(topLineSb.toString());
                 writer.newLine();
+
                 writer.write(contentLineSb.toString());
                 writer.newLine();
-                first = false;
+
+                if (last) {
+                    writer.write(lastLineSb.toString());
+                    writer.newLine();
+                }
 
             }
-            writer.write(lastLineSb.toString());
-            writer.newLine();
             writer.flush();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    private String formatContent(Row<?, ?> row, int col) {
+        Object v = row.get(col);
+        return String.format("%-12.10s", v);
+    }
+
+    private String formatHeaderContent(Object header) {
+        return String.format("#%-11.10s", header);
+    }
+
+
+    protected void writeHeader(DataContainer<?, ?> dataContainer, BufferedWriter writer) throws IOException {
+        StringBuilder contentLineSb = new StringBuilder();
+        StringBuilder topLineSb = new StringBuilder();
+        topLineSb.append(topLeftCorner);
+        int cols = dataContainer.getHeader().size();
+        int cidx = 0;
+        for (Object header : dataContainer.getHeader()) {
+            if (contentLineSb.length() == 0) {
+                contentLineSb.append(leftLine);
+            }
+            String content = formatHeaderContent(header);
+            contentLineSb.append(content);
+            for (int i = 0; i < content.length(); i++) {
+                topLineSb.append(topLine);
+            }
+            if (cidx < cols - 1) {
+                contentLineSb.append(innerVerticalLine);
+                topLineSb.append(tBottom);
+            } else {
+                topLineSb.append(topRightCorner);
+                contentLineSb.append(rightLine);
+            }
+            cidx++;
+        }
+        writer.write(topLineSb.toString());
+        writer.newLine();
+        writer.write(contentLineSb.toString());
+        writer.newLine();
+    }
+
 
     @Override
     public Map<String, String> getSettings(DataFrame dataFrame) {
