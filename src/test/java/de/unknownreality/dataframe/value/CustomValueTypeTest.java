f75a782e4d1b5f21e8fdf52ecef1da480bccc193
Alex <al.gruen@gmx.de>
2020-06-23 22:43:03 +0200

    custom value type unit tests added

--- src/test/java/de/unknownreality/dataframe/value/CustomValueTypeTest.java
+++ src/test/java/de/unknownreality/dataframe/value/CustomValueTypeTest.java
@@ -1,133 +1,97 @@
 package de.unknownreality.dataframe.value;
 
-import de.unknownreality.dataframe.column.BasicColumn;
-import de.unknownreality.dataframe.common.Row;
-import de.unknownreality.dataframe.type.ValueType;
+import de.unknownreality.dataframe.DataFrame;
+import de.unknownreality.dataframe.column.IntegerColumn;
+import de.unknownreality.dataframe.group.DataGrouping;
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.Writer;
-import java.nio.ByteBuffer;
-import java.text.ParseException;
-import java.util.Comparator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class CustomValueTypeTest {
 
     @Test
-    public void createCustomValueTypeColumn() {
+    public void customValueSortTest() {
+        DataFrame df = DataFrame.create()
+                .addColumn(new IntegerColumn("id"))
+                .addColumn(new CustomColumn("x"));
+
+        df.append(1, new CustomColumn.Custom(1, 1));
+        df.append(2, new CustomColumn.Custom(2, 2));
+        df.append(3, new CustomColumn.Custom(1, 2));
+        df.append(4, new CustomColumn.Custom(2, 1));
+
+        df = df.sort("x");
+
+        Assert.assertEquals(1, df.getRow(0).getInteger("id").intValue());
+        Assert.assertEquals(3, df.getRow(1).getInteger("id").intValue());
+        Assert.assertEquals(4, df.getRow(2).getInteger("id").intValue());
+        Assert.assertEquals(2, df.getRow(3).getInteger("id").intValue());
+    }
+
+    @Test
+    public void customValueFilter() {
+        DataFrame df = DataFrame.create()
+                .addColumn(new IntegerColumn("id"))
+                .addColumn(new CustomColumn("x"));
+
+        df.append(1, new CustomColumn.Custom(1, 1));
+        df.append(2, new CustomColumn.Custom(2, 2));
+        df.append(3, new CustomColumn.Custom(1, 2));
+        df.append(4, new CustomColumn.Custom(2, 1));
+
+        df.filter("x == '[1,2]'");
+        Assert.assertEquals(1, df.size());
+        Assert.assertEquals(3, df.getRow(0).getInteger("id").intValue());
+    }
+
+    @Test
+    public void customValueGroup() {
+        DataFrame df = DataFrame.create()
+                .addColumn(new IntegerColumn("id"))
+                .addColumn(new CustomColumn("x"));
+
+        df.append(1, new CustomColumn.Custom(1, 1));
+        df.append(2, new CustomColumn.Custom(2, 2));
+        df.append(3, new CustomColumn.Custom(1, 1));
+        df.append(4, new CustomColumn.Custom(2, 2));
+        DataGrouping g = df.groupBy("x");
+
+        Assert.assertEquals(2, g.size());
+        Assert.assertEquals(2, g.getGroup(0).size());
+        Assert.assertEquals(2, g.getGroup(1).size());
+        Assert.assertEquals(new CustomColumn.Custom(1, 1), g.getGroup(0).getGroupValues().get("x"));
+        Assert.assertEquals(new CustomColumn.Custom(2, 2), g.getGroup(1).getGroupValues().get("x"));
+    }
+
+
+    @Test
+    public void customValueJoin() {
+        DataFrame dfA = DataFrame.create()
+                .addColumn(new IntegerColumn("ida"))
+                .addColumn(new CustomColumn("x"));
+
+        dfA.append(1, new CustomColumn.Custom(1, 1));
+        dfA.append(2, new CustomColumn.Custom(2, 1));
+        dfA.append(3, new CustomColumn.Custom(1, 2));
+        dfA.append(4, new CustomColumn.Custom(2, 2));
+
+        DataFrame dfB = DataFrame.create()
+                .addColumn(new IntegerColumn("idb"))
+                .addColumn(new CustomColumn("x"));
+
+        dfB.append(4, new CustomColumn.Custom(1, 1));
+        dfB.append(3, new CustomColumn.Custom(2, 2));
+        dfB.append(2, new CustomColumn.Custom(1, 1));
+        dfB.append(1, new CustomColumn.Custom(2, 2));
+
+        DataFrame joined = dfA.joinInner(dfB, "x");
+        joined = joined.sort("ida");
+
+        Assert.assertEquals(4, joined.size());
+        Assert.assertEquals(1, joined.getRow(0).getInteger("ida").intValue());
+        Assert.assertEquals(1, joined.getRow(1).getInteger("ida").intValue());
+        Assert.assertEquals(4, joined.getRow(2).getInteger("ida").intValue());
+        Assert.assertEquals(4, joined.getRow(3).getInteger("ida").intValue());
 
     }
 
-    private class CustomColumn extends BasicColumn<Custom, CustomColumn> {
-        private CustomValueType type = new CustomValueType();
-
-        public CustomColumn() {
-            super(Custom.class);
-        }
-
-        public CustomColumn(String name, Custom[] values) {
-            super(name, values, values.length);
-        }
-
-        @Override
-        protected CustomColumn getThis() {
-            return this;
-        }
-
-        @Override
-        public ValueType<Custom> getValueType() {
-            return type;
-        }
-
-        @Override
-        public CustomColumn copy() {
-            return new CustomColumn(getName(), values);
-        }
-
-        @Override
-        public CustomColumn copyEmpty() {
-            return new CustomColumn();
-        }
-
-        @Override
-        public <H> Custom getValueFromRow(Row<?, H> row, H headerName) {
-            return row.get(headerName, Custom.class);
-        }
-
-        @Override
-        public Custom getValueFromRow(Row<?, ?> row, int headerIndex) {
-            return null;
-        }
-    }
-
-
-    private class Custom {
-        public int x;
-        public int y;
-
-        public Custom(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    private class CustomValueType extends ValueType<Custom> {
-        private Pattern PATTERN = Pattern.compile("\\[([0-9+]+),\\s*([0-9+]+)\\]");
-
-        @Override
-        public Class<Custom> getType() {
-            return Custom.class;
-        }
-
-        @Override
-        public Comparator<Custom> getComparator() {
-            return (o1, o2) -> {
-                int c = Integer.compare(o1.x, o2.y);
-                if (c == 0) {
-                    c = Integer.compare(o1.y, o2.y);
-                }
-                return c;
-            };
-        }
-
-        @Override
-        public Custom read(DataInputStream dis) throws IOException {
-            return new Custom(dis.readInt(), dis.readInt());
-        }
-
-        @Override
-        public Custom read(ByteBuffer buf) {
-            return new Custom(buf.getInt(), buf.getInt());
-        }
-
-        @Override
-        public Custom parse(String s) throws ParseException {
-            Matcher m = PATTERN.matcher(s);
-            if (m.matches()) {
-                return new Custom(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)));
-            }
-            throw new ParseException(String.format("error parsing %s: no match found"), 0);
-        }
-
-        @Override
-        public void write(Writer writer, Custom value) throws IOException {
-            writer.write(toString(value));
-        }
-
-        @Override
-        public String toString(Custom value) {
-            return String.format("[%d,%d]", value.x, value.y);
-        }
-
-        @Override
-        public int write(DataOutputStream dos, Custom value) throws IOException {
-            dos.writeInt(value.x);
-            dos.writeInt(value.y);
-            return 2 * Integer.BYTES;
-        }
-    }
 }
